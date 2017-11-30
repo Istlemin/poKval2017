ad03ce73084d82fd507ebb22b122a258f38da578
Istlemin <fredrik.w.ekholm@gmail.com>
2017-11-30 11:38:31 +0100

    Explanations for D and E added

--- poKval2017D.cpp
+++ poKval2017D.cpp
@@ -9,6 +9,31 @@ typedef long long ll;
 typedef vector<ll> vi;
 typedef pair<ll,ll> pii;
 
+/*
+Ifall man ser spelet som en riktad graf, där varje nod 
+representerar ett state i spelet, och varje kant från nod
+a till b representerar ett drag som man kan göra för att
+komma från a till b. Då kan man göra en BFS i grafen från
+start-statet till målstatet. Tidskomplexiteten för BFS är
+O(antal kanter+antal noder), vilket går då antalet kanter är
+ca N*2^N där N är längden på sekvensen, som är <= 15.
+
+Man kan även se det som att man börjar med att hitta alla
+states som ligger exakt ett drag från starten, därifrån hitta
+alla states som ligger exakt två drag från starten o.s.v.
+När man kommer till målstatet avbryter man.
+
+Ett smidigt sätt att implementera detta på är med så kallade
+bitmasks. Man representerar varje state med ett tal melan
+0 och 2^N, där talets representation i bas 2 beskriver statet.
+Då det finns inbyggt många operationer för (snabbt) manipulera
+bitarna för ett tal går det relativt enkelt att utföra dragen.
+Dessutom gör detta det enklare att hålla koll på vilka states
+du redan utforskat, då man enkelt kan använda ett state som
+index i en array.
+
+*/
+
 int main(){
 	cin.sync_with_stdio(false);
     string str; cin>>str;
--- poKval2017E.cpp
+++ poKval2017E.cpp
@@ -9,6 +9,39 @@ typedef long long ll;
 typedef vector<ll> vi;
 typedef pair<ll,ll> pii;
 
+/*
+Vi definerar en funktion f(I(heltal), catalogs(mängd))
+som svarar på frågan "ifall vi bara får titta
+på videor som ligger efter index I i listan, vi
+har redan tittat på alla kataloger i mängden catalogs,
+vad är den kortaste tiden som vi härifrån kan titta på en video
+från varje katalog."
+
+Funktionens värde för f(0,tom mängd) är svaret vi söker.
+
+För varje video kan man välja att antingen inte kolla på den
+eller kolla på den, och vi kan därför räkna ut värdet på f
+rekursivt genom att testa de två fallen och välja det som ger
+lägst svar:
+
+f(I,catalogs) =
+	min av
+		1: (längden på video I) + f(i+1, (catalogs) union (katalogerna för videon på index I)) (Fallet då man tittar på videon på index I)
+		2: f(i+1, catalogs) (Fallet då man inte tittar på videon på index I)
+
+
+Basfallen blir
+
+f(I,Full mängd) = 0 (ifall man kollat på alla kategorier redan behöver man inte kolla på några fler)
+f(N,inte full mängd) = oändligt (ifall man gått igenom alla videos men inte sätt alla kategorier går det aldrig att se alla)
+
+I koden representeras f(I,catalogs) som dp[I][catalogs]
+catalogs representeras som en bitmask,
+för att enkelt kunna använda det som ett index i en vektor.
+
+
+*/
+
 int main(){
 	cin.sync_with_stdio(false);
 	ll n;
